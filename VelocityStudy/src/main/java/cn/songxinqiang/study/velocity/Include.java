b74a2d59b971c9efe6117f57977f563bc2431382
宋信强 <songxinqiang@vip.qq.com>
2015-11-09 14:49:08 +0800

    宏定义、包含、注释

---
+++ VelocityStudy/src/main/java/cn/songxinqiang/study/velocity/Include.java
@@ -0,0 +1,60 @@
+/**
+ * <pre>
+ * Copyright 2014,2015 阿信sxq(songxinqiang@vip.qq.com).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </pre>
+ */
+/*
+ * 创建时间：2015年11月9日--下午1:51:25
+ * 作者：阿信sxq(songxinqiang@vip.qq.com)
+ */
+package cn.songxinqiang.study.velocity;
+
+import java.io.StringWriter;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+/**
+ *
+ * <p>
+ * 众里寻她千百度, 蓦然回首, 那人却在灯火阑珊处.
+ * </p>
+ *
+ * @author 阿信sxq-2015年11月9日
+ *
+ */
+public class Include {
+
+    public static void main(String[] args) {
+        VelocityEngine ve = new VelocityEngine();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+
+        ve.init();
+
+        Template t = ve.getTemplate("include.vm", "UTF-8");
+        VelocityContext ctx = new VelocityContext();
+
+        StringWriter sw = new StringWriter();
+
+        t.merge(ctx, sw);
+
+        System.out.println(sw.toString());
+    }
+
+}
---
+++ VelocityStudy/src/main/java/cn/songxinqiang/study/velocity/Macro.java
@@ -0,0 +1,62 @@
+/**
+ * <pre>
+ * Copyright 2014,2015 阿信sxq(songxinqiang@vip.qq.com).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </pre>
+ */
+/*
+ * 创建时间：2015年11月9日--下午12:00:28
+ * 作者：阿信sxq(songxinqiang@vip.qq.com)
+ */
+package cn.songxinqiang.study.velocity;
+
+import java.io.StringWriter;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+/**
+ *
+ * <p>
+ * 众里寻她千百度, 蓦然回首, 那人却在灯火阑珊处.
+ * </p>
+ *
+ * @author 阿信sxq-2015年11月9日
+ *
+ */
+public class Macro {
+
+    public static void main(String[] args) {
+        VelocityEngine ve = new VelocityEngine();
+        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+
+        ve.init();
+
+        Template t = ve.getTemplate("macro.vm");
+        VelocityContext ctx = new VelocityContext();
+
+        ctx.put("ctl", true);
+
+        StringWriter sw = new StringWriter();
+
+        t.merge(ctx, sw);
+
+        System.out.println(sw.toString());
+    }
+
+}
