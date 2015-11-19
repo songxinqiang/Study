bca5ba302bb43e4810219527f99572ff3bb521ed
宋信强 <songxinqiang@vip.qq.com>
2015-11-19 18:08:31 +0800

    velocitylayout

---
+++ VelocityStudy/src/main/java/cn/songxinqiang/study/velocity/servlet/VelocityLayout.java
@@ -0,0 +1,38 @@
+/**
+ * <pre>
+ * Copyright 2014,2015 阿信sxq(songxinqiang@vip.qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </pre>
+ */
+/*
+ * 创建时间：2015年11月19日--上午10:04:09
+ * 作者：阿信sxq(songxinqiang@vip.qq.com)
+ */
+package cn.songxinqiang.study.velocity.servlet;
+
+import org.apache.velocity.tools.view.VelocityLayoutServlet;
+
+
+/**
+ *
+ * <p>
+ * 众里寻她千百度, 蓦然回首, 那人却在灯火阑珊处.
+ * </p>
+ *
+ * @author 阿信sxq-2015年11月19日
+ *
+ */
+public class VelocityLayout extends VelocityLayoutServlet{
+
+}
---
+++ VelocityStudy/src/test/java/cn/songxinqiang/study/velocity/test/TestDateOutput.java
@@ -0,0 +1,48 @@
+/**
+ * <pre>
+ * Copyright 2014,2015 阿信sxq(songxinqiang@vip.qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * </pre>
+ */
+/*
+ * 创建时间：2015年11月19日--上午9:58:12
+ * 作者：阿信sxq(songxinqiang@vip.qq.com)
+ */
+package cn.songxinqiang.study.velocity.test;
+
+import java.util.Date;
+
+
+/**
+ *
+ * <p>
+ * 众里寻她千百度, 蓦然回首, 那人却在灯火阑珊处.
+ * </p>
+ *
+ * @author 阿信sxq-2015年11月19日
+ *
+ */
+public class TestDateOutput {
+    
+    /**
+     *
+     * @author 阿信sxq-2015年11月19日
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+
+}
